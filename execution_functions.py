26e25e12cd90565226b3f23d0af13e41ac57ea92
srikarpolisetty-svg <srikarpolisetty@gmail.com>
2026-01-19 15:02:12 -0500

    fix

--- execution_functions.py
+++ execution_functions.py
@@ -14,7 +14,7 @@ def to_float(x, default=0.0):
 
 con = duckdb.connect("stocks_data.db")
 
-def get_all_symbols(con, table="stock_bars_enriched_5m_3d"):
+def get_all_symbols(con, table="stock_bars_enriched_5m"):
     df = con.execute(
         f"SELECT DISTINCT symbol FROM {table}"
     ).df()
