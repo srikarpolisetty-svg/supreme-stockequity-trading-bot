abad1358dc16d2bd535d7dae594e97660efc384c
srikarpolisetty-svg <srikarpolisetty@gmail.com>
2026-01-21 18:53:33 -0500

    fix

--- backtest.py
+++ backtest.py
@@ -5,12 +5,32 @@ con = duckdb.connect(
     read_only=True,
 )
 
+import duckdb
+con = duckdb.connect("/home/ubuntu/supreme-stockequity-trading-bot/stocks_data.db", read_only=True)
+
+print(con.execute("PRAGMA table_info('stock_bars_raw_5m')").df()[["cid","name","type"]])
+print(con.execute("PRAGMA table_info('stock_bars_enriched_5m')").df()[["cid","name","type"]])
+print(con.execute("""
+    SELECT timestamp, symbol, open, high, low, close, volume, range_pct, con_id, snapshot_id
+    FROM stock_bars_raw_5m
+    WHERE symbol='SBAC'
+    ORDER BY timestamp DESC
+    LIMIT 5
+""").df())
+
+print(con.execute("""
+    SELECT MIN(close) mn, MAX(close) mx, COUNT(*) n
+    FROM stock_bars_raw_5m
+    WHERE symbol='SBAC'
+""").df())
+
+con.close()
 
 
-df = backtest_returns_stock_5m(
-    con,
-    symbol="SBAC",
-)
+#df = backtest_returns_stock_5m(
+    #con,
+    #symbol="SBAC",
+#)
 
-print(len(df))
-print(df.head())
+#print(len(df))
+#print(df.head())
